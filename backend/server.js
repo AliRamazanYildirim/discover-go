6e663cf1f523001d585e56c0967918dacda628bc
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-01-16 19:27:44 +0100

    Refactor event management: move routes to separate file and update model structure

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Event from './models/events.js';
-import mongoose from 'mongoose';
+import eventRoutes from './routes/event.route.js';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -16,73 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/api/events', async (req, res) => {
-    try {
-        const events = await Event.find({});
-        res.status(200).json({ success: true, data: events });
-    } catch (error) {
-        console.error("Error in Fetching events:", error.message);
-        res.status(500).json({ success: false, message: "Server Error", error: error.message });
-    }
-});
-
-app.post('/api/events', async (req, res) => {
-    const event = req.body; // user will send this data
-    const { title, description, date, location } = event;
-
-    if (!title || !description || !date || !location || !location.lat || !location.lng) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-
-    const newEvent = new Event(event);
-
-    try {
-        await newEvent.save();
-        res.status(201).json({ success: true, data: newEvent });
-    } catch (error) {
-        console.error("Error in Create event:", error.message);
-        res.status(500).json({ success: false, message: "Server Error", error: error.message });
-    }
-});
-
-app.put('/api/events/:id', async (req, res) => { 
-    const { id } = req.params;
-    const event = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-
-    try {
-        const updatedEvent = await Event.findByIdAndUpdate(id, event, { new: true });
-        if (!updatedEvent) {
-            return res.status(404).json({ success: false, message: "Event not found" });
-        }
-
-        res.status(200).json({ success: true, data: updatedEvent });
-    } catch (error) {
-        console.error("Error in Update event:", error.message);
-        res.status(500).json({ success: false, message: "Server Error", error: error.message });
-    }
-});
-
-app.delete('/api/events/:id', async (req, res) => {
-    const { id } = req.params;
-    console.log('id', id);
-
-    try {
-        const event = await Event.findByIdAndDelete(id);
-        if (!event) {
-            return res.status(404).json({ success: false, message: "Event not found" });
-        }
-
-        res.status(200).json({ success: true, message: "Event deleted" });
-    } catch (error) {
-        console.error("Error in Delete event:", error.message);
-        res.status(500).json({ success: false, message: "Server Error", error: error.message });
-    }
-});
-
+app.use('/api/events', eventRoutes);
 
 // Centralized Error Handling Middleware
 app.use((err, res) => {
